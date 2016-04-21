41f06631d1b93483d15978b96fca16efa56372b5
Oleg Burov <oleg.burov@outlook.com>
2016-04-20 19:36:54 -0700

    Implemented the Test MongoDB Connection console app.

---
+++ src/TestMongoDBConnection/Program.cs
@@ -0,0 +1,67 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace TestMongoDBConnection
+{
+  class Program
+  {
+    private static readonly StringBuilder Log = new StringBuilder();
+
+    private static void Main()
+    {
+      foreach (ConnectionStringSettings setting in ConfigurationManager.ConnectionStrings)
+      {
+        if (!setting.Name.Equals("LocalSqlServer"))
+        {
+          TestConnectionString(setting.Name, setting.ConnectionString);
+        }
+      }
+
+      DumpLog(Log);
+
+      Console.WriteLine("Press any key to close.");
+      Console.ReadKey();
+    }
+
+    private static void TestConnectionString(string name, string connectionString)
+    {      
+      Log.AppendLine();
+      Log.AppendLine($"====== Test Date: {DateTime.Now.ToString(CultureInfo.InvariantCulture)} ======");
+      Log.AppendLine();
+      Log.AppendLine($"Connection Name: {name}.");
+      Log.AppendLine($"Connection String: {connectionString}.");
+
+      try
+      {
+        MongoClient client = new MongoClient(connectionString);
+
+        var versionString = client.GetServer().BuildInfo.VersionString;
+
+        Log.AppendLine();
+        Log.AppendLine($"MongoDB version: {versionString}.");
+        Log.AppendLine("Connect is successful.");
+        Log.AppendLine();
+      }
+      catch (Exception exception)
+      {
+        Log.AppendLine($"{exception.Message}{exception.InnerException}{exception.StackTrace}");
+      }
+    }
+
+    private static void DumpLog(StringBuilder log)
+    {
+      Console.WriteLine(log);
+
+      var fileName = $"Support.Log.{DateTime.Now.ToString("yyyyMMdd.hhmmss")}.txt";
+
+      using (StreamWriter writer = new StreamWriter(fileName))
+      {
+        writer.Write(log);
+      }       
+    }
+  }
+}
---
+++ src/TestMongoDBConnection/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.TestMongoDBConnection")]
+[assembly: AssemblyProduct("Sitecore.Support.TestMongoDBConnection")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0 rev. 160420")]
+
+[assembly: AssemblyDescription("Sitecore Support Diagnostics")]
+[assembly: AssemblyCompany("Sitecore Corporation")]
+[assembly: AssemblyCopyright("Copyright © 2016 Sitecore Corporation")]
+[assembly: AssemblyTrademark("Sitecore® is a registered trademark of Sitecore Corporation")]
+[assembly: ComVisible(false)]
+
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("893d8b32-7123-47ad-86cd-df5ce60bd4d9")]
